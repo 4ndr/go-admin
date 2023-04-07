e5610fd67f2fd0bc77bce0699699440d76d9f104
andre leite <andre_leite@comcast.com>
2023-04-07 10:29:51 +0100

    First Commit
    Signed-off-by: andre leite <andre_leite@comcast.com>

---
+++ routes/routes.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"github.com/4ndr/go-admin/controllers"
+	"github.com/4ndr/go-admin/middlewares"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func Setup(app *fiber.App) {
+	app.Post("/api/register", controllers.Register)
+	app.Post("/api/login", controllers.Login)
+
+	app.Use(middlewares.IsAuthenticated)
+
+	app.Get("/api/user", controllers.User)
+	app.Post("/api/logout", controllers.Logout)
+}
