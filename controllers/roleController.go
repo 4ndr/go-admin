37e777a1adecebcacd93a5b55ccdf504d83c0a9b
4ndr <andrepereiraleite@gmail.com>
2023-04-21 09:44:34 +0100

    Added Admin features
    Signed-off-by: 4ndr <andrepereiraleite@gmail.com>

---
+++ controllers/roleController.go
@@ -0,0 +1,100 @@
+package controllers
+
+import (
+	"strconv"
+
+	"github.com/4ndr/go-admin/database"
+	"github.com/4ndr/go-admin/models"
+	"github.com/gofiber/fiber/v2"
+)
+
+func AllRoles(c *fiber.Ctx) error {
+	var roles []models.Role
+
+	database.DB.Find(&roles)
+
+	return c.JSON(roles)
+}
+
+func CreateRole(c *fiber.Ctx) error {
+	var roleDto fiber.Map
+
+	if err := c.BodyParser(&roleDto); err != nil {
+		return err
+	}
+
+	list := roleDto["permissions"].([]interface{})
+	permissions := make([]models.Permission, len(list))
+
+	for i, permissionId := range list {
+		id, _ := strconv.Atoi(permissionId.(string))
+		permissions[i] = models.Permission{
+			Id: uint(id),
+		}
+	}
+
+	role := models.Role{
+		Name:       roleDto["name"].(string),
+		Permission: permissions,
+	}
+
+	database.DB.Create(&role)
+
+	return c.JSON(role)
+}
+
+func GetRole(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	role := models.Role{
+		Id: uint(id),
+	}
+
+	database.DB.Find(&role)
+
+	return c.JSON(role)
+}
+
+func UpdateRole(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	var roleDto fiber.Map
+
+	if err := c.BodyParser(&roleDto); err != nil {
+		return err
+	}
+
+	list := roleDto["permissions"].([]interface{})
+	permissions := make([]models.Permission, len(list))
+
+	for i, permissionId := range list {
+		id, _ := strconv.Atoi(permissionId.(string))
+		permissions[i] = models.Permission{
+			Id: uint(id),
+		}
+	}
+
+	database.DB.Exec("DELETE from role_permission WHERE role_id = ?", id)
+
+	role := models.Role{
+		Id:         uint(id),
+		Name:       roleDto["name"].(string),
+		Permission: permissions,
+	}
+
+	database.DB.Model(&role).Updates(role)
+
+	return c.JSON(role)
+}
+
+func DeleteRole(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	role := models.Role{
+		Id: uint(id),
+	}
+
+	database.DB.Delete(&role)
+
+	return nil
+}
